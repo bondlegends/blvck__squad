d07697f58e3136cf73d5b861ebcd7268e919a33e
bondlegends <botsetup7@gmail.com>
2024-11-06 11:49:01 +0530

    Update valid.js

--- valid.js
+++ valid.js
@@ -1,11 +1,11 @@
 function myfunction(){
     var x =document.getElementById("pass");
 
-    if(x.type === "blacksquad"){
+    if(x.type === "password"){
         x.type = "text";
     }
     else{
-        x.type = "123";
+        x.type = "password";
     }
 }
 
